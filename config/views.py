70c5db4c1ca18de5c20b13be8c4c2454fa049d8e
avrahamyoon <abrahampeking@gmail.com>
2026-07-01 10:13:51 -0600

    Modernize UI

--- config/views.py
+++ config/views.py
@@ -1,5 +1,11 @@
+from django.contrib import admin
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(admin.site.each_context(self.request))
+        return context
